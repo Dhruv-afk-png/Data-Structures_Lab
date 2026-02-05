bf0cce287e282d7f487a888a85d54c11d0ee1d76
Dhruv-afk-png <dhruv.bhonde.batch2025@sitnagpur.siu.edu.in>
2026-02-05 11:13:59 +0530

    Update Linear_Search.c

--- Linear Search/Linear_Search.c
+++ Linear Search/Linear_Search.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, key, i, found = 0;
+    scanf("%d", &n);
+    int arr[n];
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &key);
+
+    for(i=0;i<n;i++){
+		if(arr[i] == key){
+			found = 1;
+			break;
+		}
+	}
+	if(found==1){
+		printf("found at position %d\n", i);
+	}else{
+		printf("%d not found\n", key);
+	}
+}
 
