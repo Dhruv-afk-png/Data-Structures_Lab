4d9fbb55a3487f4156a5988828e86e1b60a7439f
Dhruv-afk-png <dhruv.bhonde.batch2025@sitnagpur.siu.edu.in>
2026-03-11 21:44:49 +0530

    Update Conc_opp.c

--- Experiment4/Conc Opp SLL/Conc_opp.c
+++ Experiment4/Conc Opp SLL/Conc_opp.c
@@ -1,1 +1,2 @@
-
+/*Write a C program to implement a menu-driven application for performing operations on two Singly Linked Lists. 
+The program should allow the user to create two singly linked lists, display them, and concatenate the second list to the end of the first list.*/
